d85335269d6607491943121383405b450259aaac
MilosRandjelovic <mrandjelovic1979@gmail.com>
2024-10-06 13:19:39 +0200

    Finish all stuffs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-export default nextConfig;
+// Get the current file's directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname); // Now __dirname works in .mjs
+    return config;
+  },
+};
